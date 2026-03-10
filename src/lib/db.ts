05433109bad5c2fe08a6dbed952487c45ecaf56a
Moi-przs <243690@ids.upchiapas.edu.mx>
2026-03-10 02:52:18 -0600

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,11 @@
 import { Pool, QueryResult } from 'pg';
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: 'localhost', 
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5433,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST, 
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
 
 export const query = (text: string, params?: unknown[]): Promise<QueryResult> => {
